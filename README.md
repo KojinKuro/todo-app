2676ac5549e4abba190841ff60575ba28ceddf2e
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-11 22:13:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# To Do App
+
+Technologies used:
+
+- Webpack
+- HTML
+- Sass
+- CSS
+- Javascript
+
+## Description
+
+This is a To Do App combing what I perceived to be the best features from popular to do applications into one application.
+
+## Preview
+
+![No image]()
+
+## Features
+
+- Add tasks
+- Track them
+
+## Learning Goals
+
+This is a To Do App created to practice Webpack, implementing Functional Programming design concepts, and general problem solving skills.
+
+## Context
+
+This project is still ongoing.
+
+## Future Roadmap
+
+- To be decided
---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(__dirname, "src"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext][query]",
+        },
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
