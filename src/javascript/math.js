7ba2955658063fbe04d2afac3d6627f260309b55
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-17 20:42:07 -0700

    Generate random IDs
    A method for creating random IDs has been created

--- src/javascript/math.js
+++ src/javascript/math.js
@@ -7,3 +7,18 @@ export function randomNumber(min, max) {
   const randomNumber = Math.floor(Math.random() * (range + 1)) + min;
   return randomNumber;
 }
+
+export function generateRandomID(identifierLength) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const charString = alphabet.toLowerCase() + alphabet.toUpperCase() + numbers;
+  const timeStamp = Date.now().toString();
+
+  let randomID = "";
+  for (let i = 0; i < identifierLength; ++i) {
+    let randomIndex = randomNumber(0, charString.length - 1);
+    randomID += charString.at(randomIndex);
+  }
+
+  return randomID + timeStamp;
+}
