5918adfabcbbc5ceb81effb745ecc330f0c6a582
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-03-17 20:22:25 -0700

    Add random number generator

--- src/javascript/math.js
+++ src/javascript/math.js
@@ -1,3 +1,9 @@
 export function clamp(val, min, max) {
   return Math.min(Math.max(val, min), max);
 }
+
+export function randomNumber(min, max) {
+  const range = max - min;
+  const randomNumber = Math.floor(Math.random() * (range + 1)) + min;
+  return randomNumber;
+}
